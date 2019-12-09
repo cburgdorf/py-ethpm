1b3ef89ede95d9efa20c512daf665b4d73c70af5
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2019-12-09 11:56:26 +0100

    Add Matomo Tracking to Docs site.
    Matomo is an Open Source web analytics platform that allows us
    to get better insights and optimize for our audience without
    the negative consequences of other compareable platforms.
    
    Read more: https://matomo.org/why-matomo/

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+def setup(app):
+    app.add_js_file("js/matomo.js")
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
